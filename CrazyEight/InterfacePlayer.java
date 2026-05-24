fffab2f0a53c630c22ec0231f2b8dd39d22d307a
ㅤZhxck <zhacxk@gmail.com>
2026-05-24 19:25:19 +0700

    changes: add crazy eight original

--- CrazyEight/InterfacePlayer.java
+++
@@ -1,7 +0,0 @@
-public interface InterfacePlayer {
-    String getName();
-    Hand getHand();
-    Card play(Card leadingCard, CardCollection drawPile);
-    boolean hasSuit(int suit);
-    void display();
-}
