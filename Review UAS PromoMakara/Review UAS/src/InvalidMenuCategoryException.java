594cfcdc38c11934e485a1ad640b745c46502a77
ㅤZhxck <zhacxk@gmail.com>
2026-05-24 19:29:00 +0700

    Remove Review UAS PromoMakara from tracking

--- Review UAS PromoMakara/Review UAS/src/InvalidMenuCategoryException.java
+++
@@ -1,9 +0,0 @@
-public class InvalidMenuCategoryException extends Exception {
-    public InvalidMenuCategoryException(){
-        super("Error: menunya gaada cik");
-    }
-
-    public InvalidMenuCategoryException(String message) {
-        super(message);
-    }
-}
